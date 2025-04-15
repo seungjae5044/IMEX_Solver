2bf11685689d118aab02062a0042604223697f08
seungjae5044 <seungjae5044@gmail.com>
2025-04-16 03:25:47 +0900

    test_2

--- test.py
+++ test.py
@@ -9,17 +9,37 @@ from tqdm.auto import tqdm
 import numpy as np
 from models import UNet, UNet_small
 
+def q_sample(x0, t, noise=None):
+    if noise is None:
+        noise = torch.randn_like(x0)
+    # 각 t에 대해 sqrt(alpha_bar)와 sqrt(1 - alpha_bar) 계산
+    sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod[t]).view(-1, 1, 1, 1)
+    sqrt_one_minus_alphas_cumprod = torch.sqrt(1 - alphas_cumprod[t]).view(-1, 1, 1, 1)
+    return sqrt_alphas_cumprod * x0 + sqrt_one_minus_alphas_cumprod * noise
+
 # Device 설정
-device = "cuda" if torch.cuda.is_available() else "cpu"
-device = "mps" if torch.backends.mps.is_available() else device
+if __name__ == "__main__":
+        
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "mps" if torch.backends.mps.is_available() else device
 
-model = UNet_small().to(device)
+    T_steps = 1000
+    beta_start = 1e-5
+    beta_end = 0.02
+    betas = torch.linspace(beta_start, beta_end, T_steps).to(device)  # (T_steps,)
+    alphas = 1.0 - betas
+    alphas_cumprod = torch.cumprod(alphas, dim=0)  # 누적곱, (T_steps,)
 
-x = torch.rand(1,6,256,256, device=device)
+    path_HR = f"data/Sun-Hays80/image_SRF_{resolution}/img_00{i}_SRF_{resolution}_HR.png"
 
 
-T_steps = 1000
-t = torch.randint(0, T_steps, (1,), device=device).long()
-y_hat = model(x, t)
-breakpoint()
+    model = UNet_small().to(device)
+
+    x = torch.rand(1,6,256,256, device=device)
+
+
+    T_steps = 1000
+    t = torch.randint(0, T_steps, (1,), device=device).long()
+    y_hat = model(x, t)
+    breakpoint()
 
